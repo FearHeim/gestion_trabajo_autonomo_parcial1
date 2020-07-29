20b2feedbd927e593d186c0a2c1dc08ccb79dcdf
FearHeim <narjos2000@gmail.com>
2020-07-28 20:13:58 -0500

    juan arregla tu wea

--- hollow-games-win32-x64/resources/app/app/database.js
+++ hollow-games-win32-x64/resources/app/app/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://proto:quacTugxMlEP5ftY@proyectocrud.3mvfq.mongodb.net/prueba54?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://proto:quacTugxMlEP5ftY@proyectocrud.3mvfq.mongodb.net/juego?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
